ba1b4c87e692687bd89c3ac4a55de5792452e7c0
johnjihwan <s20062@gsm.hs.kr>
2021-01-29 23:33:04 +0900

    [ADD]Can Use Tamplate String

---
+++ Types/string.js
@@ -0,0 +1,6 @@
+var str;
+var red = "red";
+var green = "green";
+var mycolor = 'mycolor is ${red}.';
+var iscolor = 'iscolor is' + green;
+console.log(red, green, mycolor, iscolor);
---
+++ Types/string.ts
@@ -0,0 +1,6 @@
+let str: string;
+let red: string = "red";
+let green: string = "green";
+let mycolor: string = 'mycolor is ${red}.';
+let iscolor: string = 'iscolor is'+green;
+console.log(red, green, mycolor, iscolor);
