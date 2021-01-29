1e8e46b12c6d0065f8f02c205e191afdeadf2872
johnjihwan <s20062@gsm.hs.kr>
2021-01-30 00:56:43 +0900

    [DELETE]Main.ts, Main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-var sum = add(1, 2);
-console.log(sum);
-console.log('TypeScript on NodeJS!');
--- main.ts
+++
@@ -1,7 +0,0 @@
-function add(a: number, b: number){
-    return a+b;
-}
-const sum: number = add(1,2);
-console.log(sum);
-
-console.log('TypeScript on NodeJS!');
