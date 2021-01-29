49f39e6d91abd56fb3437af4405c41c0b7589e85
johnjihwan <s20062@gsm.hs.kr>
2021-01-29 23:39:09 +0900

    [ADD]How can use array statement in Ts?

---
+++ Types/array.js
@@ -0,0 +1,7 @@
+// 문자열만 갖는 배열 
+var fruits = ['Apple', 'Orange', 'Banana'];
+var vege = ['apple', 'vege', 'mango'];
+// 숫자만 갖는 배열
+var onToSeven = [1, 2, 3, 4, 5, 6, 7];
+var oneToseven = [1, 2, 3, 4, 5, 6, 7];
+console.log(fruits, vege, oneToseven, onToSeven);
---
+++ Types/array.ts
@@ -0,0 +1,9 @@
+// 문자열만 갖는 배열 
+let fruits : string[] = ['Apple','Orange','Banana'];
+let vege : Array<string> = ['apple','vege','mango'];
+
+// 숫자만 갖는 배열
+let onToSeven: number[] = [1,2,3,4,5,6,7];
+let oneToseven: Array<number> = [1,2,3,4,5,6,7];
+
+console.log(fruits, vege, oneToseven, onToSeven);
