6e4817f7dd62cc776b65f2446763538993898dd1
johnjihwan <s20062@gsm.hs.kr>
2021-01-29 23:56:44 +0900

    [ADD]array use interface

---
+++ Types/interface.js
@@ -0,0 +1,14 @@
+var userArr = [
+    {
+        name: 'John',
+        age: 34,
+        isVailable: false
+    },
+    {
+        name: 'hwan',
+        age: 34,
+        isVailable: true
+    }
+];
+console.log(userArr);
+//interface 에서 정의한 객체를 사용하지 않으면 error 발생.
---
+++ Types/interface.ts
@@ -0,0 +1,21 @@
+interface IUser{
+    name: string;
+    age: number;
+    isVailable: boolean;
+}
+let userArr: IUser[] = [
+    {
+        name: 'John',
+        age: 34,
+        isVailable: false
+    },
+    {
+        name: 'hwan',
+        age: 34,
+        isVailable: true
+    }
+];
+
+console.log(userArr);
+
+//interface 에서 정의한 객체를 사용하지 않으면 error 발생.
