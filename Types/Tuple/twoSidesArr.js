42f12e6750d116ac79635ed397ea33c681c12f96
johnjihwan <s20062@gsm.hs.kr>
2021-01-30 00:38:45 +0900

    [ADD]Use TwoSidesArr with Tuple

---
+++ Types/Tuple/twoSidesArr.js
@@ -0,0 +1,3 @@
+var wow;
+wow = [['age', 3], ['hi', 1], ['my', 5]];
+console.log(wow);
---
+++ Types/Tuple/twoSidesArr.ts
@@ -0,0 +1,5 @@
+//Tuple 과 함께 2차원 배열 사용방법.
+let wow:[string, number][];
+wow = [['age',3], ['hi',1], ['my',5]];
+
+console.log(wow);
