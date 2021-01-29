f64f54ea282c76aa9beeb512c882f7fbce718307
johnjihwan <s20062@gsm.hs.kr>
2021-01-29 23:27:00 +0900

    [ADD]Ts can calulate leteral function

---
+++ Types/number.js
@@ -0,0 +1,6 @@
+var num;
+var integer = 6;
+var float = 3.14;
+var hex = 0xf00d;
+var binary = 10;
+console.log(hex, binary);
---
+++ Types/number.ts
@@ -0,0 +1,9 @@
+let num: number;
+let integer: number=6;
+let float: number=3.14;
+let hex:number=0xf00d;
+let binary:number=0b1010;
+
+console.log(hex, binary);
+
+// es6에 도입된 2진수 및 8진수 리터럴도 지원.
